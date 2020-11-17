746b332bf54889b0e2ee18039c0df2b219bd9295
Andrii Kiziak <andriykiziak@gmail.com>
2020-11-17 13:42:54 +0100

    deleted

--- app.js
+++
@@ -1,1 +0,0 @@
-!function(){"use strict";angular.module("DIApp",[]).controller("DIController",["$scope","$filter",function(e,n){e.name="Yaakov",e.upper=function(){var o=n("uppercase");e.name=o(e.name)}}])}();
