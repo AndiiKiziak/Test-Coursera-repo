fe5064d9c675ae518b54d2a3498419c5830ba8d3
Andrii Kiziak <andriykiziak@gmail.com>
2020-11-17 10:47:02 +0100

    First new repo

---
+++ app.js
@@ -0,0 +1,1 @@
+!function(){"use strict";angular.module("DIApp",[]).controller("DIController",["$scope","$filter",function(e,n){e.name="Yaakov",e.upper=function(){var o=n("uppercase");e.name=o(e.name)}}])}();
